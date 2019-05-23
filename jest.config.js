6a6fe1cf70461c46f0a5807c654a8facd20be84c
YOU54F <yousafn@gmail.com>
2019-05-23 02:39:41 +0100

    parallel workflows

--- jest.config.js
+++ jest.config.js
@@ -6,10 +6,7 @@ module.exports = {
       displayName: 'cypress',
       runner: './index.js',
       testMatch: ['<rootDir>/**/cypress/integration/**/*.js'],      
-    },
-    {
-      displayName: 'test' // Unit Tests with jest
-    },
+    }
     
   ]
 }
